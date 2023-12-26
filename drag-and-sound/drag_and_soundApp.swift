ca462cbefdaf3ee07ad2f592ab527a1797d6fd65
kehindeBankole <bankolek1@gmail.com>
2023-12-26 11:48:04 +0100

    Initial Commit

---
+++ drag-and-sound/drag_and_soundApp.swift
@@ -0,0 +1,17 @@
+//
+//  drag_and_soundApp.swift
+//  drag-and-sound
+//
+//  Created by kehinde on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct drag_and_soundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
